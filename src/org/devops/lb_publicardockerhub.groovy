a9784293133f83918981a26a45975ef4d70d7d18
Javvq <167478687+Javvq@users.noreply.github.com>
2025-04-16 17:28:21 -0500

    Update lb_publicardockerhub.groovy

--- src/org/devops/lb_publicardockerhub.groovy
+++ src/org/devops/lb_publicardockerhub.groovy
@@ -1,8 +1,10 @@
 package org.devops
 
 def publicarImage(projectGitName){
-    withCredentials([usernamePassword(credentialsId: 'YOUR_CREDENTIALS_ID', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-    sh 'echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin'
-    sh 'docker push javv0/react-test-jenkinsfile'
-}
+    withCredentials([usernamePassword(credentialsId: "${env.TOKEN_ID}", 
+    passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
+        sh "docker login -u ${env.DOCKERHUB_USERNAME} -p ${env.DOCKERHUB_PASSWORD}"
+        sh "docker tag ${projectGitName} ${env.DOCKERHUB_USERNAME}/${projectGitName}"
+        sh "docker push ${env.DOCKERHUB_USERNAME}/${projectGitName}"
+    }
 }
