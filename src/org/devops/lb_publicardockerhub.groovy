3fa8b131cd9d4a44ac81797d858df97ddd82820b
Javvq <167478687+Javvq@users.noreply.github.com>
2025-04-16 17:40:37 -0500

    Update lb_publicardockerhub.groovy

--- src/org/devops/lb_publicardockerhub.groovy
+++ src/org/devops/lb_publicardockerhub.groovy
@@ -1,10 +1,13 @@
 package org.devops
 
 def publicarImage(projectGitName){
-    withCredentials([usernamePassword(credentialsId: "${env.TOKEN_ID}", 
-    passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
-        sh "docker login -u ${env.DOCKERHUB_USERNAME} -p ${env.DOCKERHUB_PASSWORD}"
-        sh "docker tag ${projectGitName} ${env.DOCKERHUB_USERNAME}/${projectGitName}"
-        sh "docker push ${env.DOCKERHUB_USERNAME}/${projectGitName}"
+    withCredentials([usernamePassword(
+        credentialsId: "dockerhub-token-javv0", 
+        passwordVariable: 'DOCKERHUB_PASSWORD', 
+        usernameVariable: 'DOCKERHUB_USERNAME')]) {
+        
+        sh "docker login -u ${DOCKERHUB_USERNAME} -p ${DOCKERHUB_PASSWORD}"
+        sh "docker tag ${projectGitName} ${DOCKERHUB_USERNAME}/${projectGitName}"
+        sh "docker push ${DOCKERHUB_USERNAME}/${projectGitName}"
     }
 }
