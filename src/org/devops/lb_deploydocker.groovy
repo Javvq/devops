5ebb68f9d5cb76a28843f4f10cd7fd27a8c69ed1
Javvq <167478687+Javvq@users.noreply.github.com>
2025-04-21 10:25:29 -0500

    Update lb_deploydocker.groovy

--- src/org/devops/lb_deploydocker.groovy
+++ src/org/devops/lb_deploydocker.groovy
@@ -1,11 +1,21 @@
 package org.devops
 
 def despliegueContenedor(projectGitName){
-    //sh "docker stop ${projectGitName}"
-    //sh "docker rm ${projectGitName}"
+    // Detener y eliminar el contenedor si ya existe
+    sh """
+        if [ \$(docker ps -a -q -f name=${projectGitName}) ]; then
+            docker stop ${projectGitName} || true
+            docker rm ${projectGitName} || true
+        fi
+    """
+
+    // Asegurarse de tener la última versión
     sh "docker pull javv0/${projectGitName}"
-    sh """ docker run -d --name ${projectGitName} \
-        --network=${env.NameNetwork} -p 5174:5174 \
+
+    // Ejecutar el contenedor en red bridge (por defecto)
+    sh """
+        docker run -d --name ${projectGitName} \
+        --network bridge -p 5174:5174 \
         --user root javv0/${projectGitName}
     """
 }
