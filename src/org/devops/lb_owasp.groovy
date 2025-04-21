8927355ee6f5d4ca7101bb59de5df917067270d2
Javvq <167478687+Javvq@users.noreply.github.com>
2025-04-21 12:07:56 -0500

    Update lb_owasp.groovy

--- src/org/devops/lb_owasp.groovy
+++ src/org/devops/lb_owasp.groovy
@@ -3,7 +3,7 @@ package org.devops
 def AnalisisOwasp(projectGitName){
     sh """ docker run --rm -v ${projectGitName}Owasp:/zap/wrk/:rw \
         --user root --network=${env.NameNetwork} \
-        -t owasp/zap2docker-stable \
+        -t owasp/zap2docker-weekly \
         zap-full-scan.py \
         -t ${env.dominio} \
         -r ${projectGitName}Owasp.html -I
